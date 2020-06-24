a582dd86a32e59016fa8f577c7045c33d5a348ab
stefanoPoleggi <stefano.poleggi@gmail.com>
2020-06-24 18:59:23 -0400

    first commit

---
+++ contiki/sensors/Air/build/native/obj/coap.d
@@ -0,0 +1,60 @@
+build/native/obj/coap.o: ../../../../os/net/app-layer/coap/coap.c \
+ ../../../../os/net/app-layer/coap/module-macros.h \
+ ../../../../os/sys/cc.h ../../../../os/contiki.h \
+ ../../../../arch/platform/native/./contiki-conf.h \
+ ../../../../arch/cpu/native/./native-def.h \
+ ../../../../os/contiki-default-conf.h ../../../../os/sys/process.h \
+ ../../../../os/sys/pt.h ../../../../os/sys/lc.h \
+ ../../../../os/sys/lc-switch.h ../../../../os/sys/autostart.h \
+ ../../../../os/sys/process.h ../../../../os/sys/timer.h \
+ ../../../../os/sys/clock.h ../../../../os/sys/ctimer.h \
+ ../../../../os/sys/etimer.h ../../../../os/sys/etimer.h \
+ ../../../../os/sys/rtimer.h ../../../../os/dev/watchdog.h \
+ ../../../../arch/cpu/native/./rtimer-arch.h ../../../../os/sys/pt.h \
+ ../../../../os/sys/clock.h ../../../../os/sys/cc-gcc.h \
+ ../../../../os/net/app-layer/coap/coap.h \
+ ../../../../os/net/app-layer/coap/coap-constants.h \
+ ../../../../os/net/app-layer/coap/coap-conf.h \
+ ../../../../os/net/app-layer/coap/coap-transport.h \
+ ../../../../os/net/app-layer/coap/coap-endpoint.h \
+ ../../../../os/net/ipv6/uip.h ../../../../os/net/ipv6/uipopt.h \
+ ../../../../os/net/ipv6/uipbuf.h ../../../../os/net/linkaddr.h \
+ ../../../../os/net/ipv6/tcpip.h \
+ ../../../../os/net/app-layer/coap/coap-transactions.h \
+ ../../../../os/net/app-layer/coap/coap-engine.h \
+ ../../../../os/net/app-layer/coap/coap-timer.h \
+ ../../../../os/net/app-layer/coap/coap-observe.h \
+ ../../../../os/net/app-layer/coap/coap-separate.h \
+ ../../../../os/net/app-layer/coap/coap-observe-client.h \
+ ../../../../os/net/app-layer/coap/coap-log.h ../../../../os/sys/log.h \
+ ../../../../os/sys/log-conf.h
+../../../../os/net/app-layer/coap/coap.c :
+ ../../../../os/net/app-layer/coap/module-macros.h :
+ ../../../../os/sys/cc.h ../../../../os/contiki.h :
+ ../../../../arch/platform/native/./contiki-conf.h :
+ ../../../../arch/cpu/native/./native-def.h :
+ ../../../../os/contiki-default-conf.h ../../../../os/sys/process.h :
+ ../../../../os/sys/pt.h ../../../../os/sys/lc.h :
+ ../../../../os/sys/lc-switch.h ../../../../os/sys/autostart.h :
+ ../../../../os/sys/process.h ../../../../os/sys/timer.h :
+ ../../../../os/sys/clock.h ../../../../os/sys/ctimer.h :
+ ../../../../os/sys/etimer.h ../../../../os/sys/etimer.h :
+ ../../../../os/sys/rtimer.h ../../../../os/dev/watchdog.h :
+ ../../../../arch/cpu/native/./rtimer-arch.h ../../../../os/sys/pt.h :
+ ../../../../os/sys/clock.h ../../../../os/sys/cc-gcc.h :
+ ../../../../os/net/app-layer/coap/coap.h :
+ ../../../../os/net/app-layer/coap/coap-constants.h :
+ ../../../../os/net/app-layer/coap/coap-conf.h :
+ ../../../../os/net/app-layer/coap/coap-transport.h :
+ ../../../../os/net/app-layer/coap/coap-endpoint.h :
+ ../../../../os/net/ipv6/uip.h ../../../../os/net/ipv6/uipopt.h :
+ ../../../../os/net/ipv6/uipbuf.h ../../../../os/net/linkaddr.h :
+ ../../../../os/net/ipv6/tcpip.h :
+ ../../../../os/net/app-layer/coap/coap-transactions.h :
+ ../../../../os/net/app-layer/coap/coap-engine.h :
+ ../../../../os/net/app-layer/coap/coap-timer.h :
+ ../../../../os/net/app-layer/coap/coap-observe.h :
+ ../../../../os/net/app-layer/coap/coap-separate.h :
+ ../../../../os/net/app-layer/coap/coap-observe-client.h :
+ ../../../../os/net/app-layer/coap/coap-log.h ../../../../os/sys/log.h :
+ ../../../../os/sys/log-conf.h :
---
+++ contiki/sensors/Air/resources/res_air.c
@@ -0,0 +1,124 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "contiki.h"
+#include "coap-engine.h"
+#include "coap-blocking-api.h"
+#include "random.h"
+
+//Log configuration
+#include "sys/log.h"
+#define LOG_MODULE "Air sensor"
+#define LOG_LEVEL LOG_LEVEL_DBG
+
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_event_handler(void);
+
+double carbon_dioxide_level = 0.03; //percentage of carbon dioxide present in the air
+static int counter = 0; //number of time observing function is triggered
+enum levels {GREEN, YELLOW, RED};
+unsigned long timest;
+extern bool purification_mode;
+enum levels alert_level = GREEN;
+
+
+EVENT_RESOURCE(res_air,
+		       "title=\"Air sensor\";methods=\"GET\";rt=\"float\";obs\n",
+		        res_get_handler,
+		        NULL,
+		        NULL,
+			NULL,
+		        res_event_handler);
+
+static void res_event_handler(void){
+	double increment = ((double)rand()/RAND_MAX)*0.004+0.001; //random increment between 0.001 and 0.005
+	counter++;	
+	if(purification_mode){
+		if(alert_level == GREEN){
+			carbon_dioxide_level = 0.03;
+			LOG_INFO("PURIFICATION ON GREEN");
+		}else if(alert_level == YELLOW){
+			carbon_dioxide_level += increment-0.002;
+			LOG_INFO("PURIFICATION ON YELLOW");
+		}else if(alert_level == RED){
+			carbon_dioxide_level += increment-0.004;
+			LOG_INFO("PURIFICATION ON RED");
+		}
+	}else{
+		carbon_dioxide_level += increment;
+	}	
+	if(carbon_dioxide_level < 0.03){
+		carbon_dioxide_level = 0.03;
+	}
+	if(carbon_dioxide_level >= 0.03 && carbon_dioxide_level <= 0.08){
+
+		alert_level = GREEN;
+	}else if (carbon_dioxide_level > 0.08 && carbon_dioxide_level <= 0.20){
+
+		alert_level = YELLOW;
+	}else if (carbon_dioxide_level > 0.20){
+
+		alert_level = RED;
+	}
+	
+	//notify all observer
+	coap_notify_observers(&res_air);
+	LOG_DBG("observing_sender");
+
+}
+
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
+	unsigned int accept = APPLICATION_JSON;
+	coap_get_header_accept(request, &accept);
+	
+	timest = (unsigned long)time(NULL);
+		
+	if(accept == APPLICATION_JSON) {
+		coap_set_header_content_format(response, APPLICATION_JSON);
+		snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "{\"carbon_dioxide\":%lf,\"timestamp\":%lu}", carbon_dioxide_level, timest);
+		coap_set_payload(response, buffer, strlen((char *)buffer));
+    	}else{
+		coap_set_status_code(response, NOT_ACCEPTABLE_4_06);
+		const char *msg = "Supporting content-types application/json";
+		coap_set_payload(response, msg, strlen(msg));
+	}
+    
+}
+
+
+
+/*if(accept == TEXT_PLAIN){
+		coap_set_header_content_format(response, TEXT_PLAIN);
+		switch(alert_level){
+			case GREEN: coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "EVENT %lu, carbon_dioxide_level: %lf, timestamp: %lu, level_ %s", (unsigned long) counter, carbon_dioxide_level, timest, "GREEN"));break;
+			case YELLOW: coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "EVENT %lu, carbon_dioxide_level: %lf, timestamp: %lu, level_ %s", (unsigned long) counter, carbon_dioxide_level, timest, "YELLO"));break;
+			case RED: coap_set_payload(response, buffer, snprintf((char *)buffer, preferred_size, "EVENT %lu, carbon_dioxide_level: %lf, timestamp: %lu, level_ %s", (unsigned long) counter, carbon_dioxide_level, timest, "RED"));
+			
+		}
+  		
+	}else if(accept == APPLICATION_XML) {
+		coap_set_header_content_format(response, APPLICATION_XML);
+		snprintf((char *)buffer, COAP_MAX_CHUNK_SIZE, "<carbon_dioxide=\"%lf\"/>", carbon_dioxide_level);
+		coap_set_payload(response, buffer, strlen((char *)buffer));
+  	}else*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
